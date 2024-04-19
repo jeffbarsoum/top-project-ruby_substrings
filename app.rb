5e2739e0950cbdee56fd84a082971c6f6ba4771b
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-04-18 21:06:53 -0400

    Refactored Code

--- app.rb
+++ app.rb
@@ -3,12 +3,35 @@ def test_dictionary
   dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 end
 
+###############################################################################
+## Helper Functions
+###############################################################################
 
-def substrings(string, dictionary)
-  dictionary.reduce(Hash.new(0)) do |hash, substring|
-    hash[substring] += string.downcase.scan(/#{substring}/).length if string.downcase.include? substring.downcase
+def count_pattern string, pattern
+  string.downcase.scan(/#{pattern}/).length
+end
+
+def string_includes_pattern? string, pattern
+  string.downcase.include? pattern.downcase
+end
+
+
+###############################################################################
+## Main Function
+###############################################################################
+
+def substrings string, dictionary
+  dictionary.reduce Hash.new 0 do |hash, substring|
+    if string_includes_pattern? string, substring
+      hash[substring] += count_pattern string, substring
+    end
     hash
   end
 end
+
+
+###############################################################################
+## Call Function
+###############################################################################
 
 substrings  "Howdy partner, sit down! How's it going?", test_dictionary
